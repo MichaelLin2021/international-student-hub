0a7450d39bd018d5106fdff1211f043277dd55d2
Qi Lin <Michaellin012@gmail.com>
2026-05-02 04:39:50 +0100

    nav bar

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function searchInfo() {
     alert("Search feature will be implemented in future version.");
 }
+
+function changeLanguage(language) {
+    if (language === "en") {
+        alert("English selected.");
+    } else if (language === "zh") {
+        alert("Chinese selected. Full translation will be available in a future version.");
+    } else if (language === "ar") {
+        alert("Arabic selected. Full translation will be available in a future version.");
+    } else if (language === "hi") {
+        alert("Hindi selected. Full translation will be available in a future version.");
+    } else if (language === "vi") {
+        alert("Vietnamese selected. Full translation will be available in a future version.");
+    }
+}
